889075802c64af4e86c257141f615a6900601fb5
denzoh <dennismwangi4567@gmail.com>
2024-03-13 17:34:58 +0300

    final product

--- src/App.js
+++ src/App.js
@@ -7,17 +7,35 @@ import Features from './components/Features';
 import ChooseUs from './components/ChooseUs';
 import Course from './components/Course';
 import Footer from './components/Footer';
+import {useState, useEffect} from 'react'
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const [loading, setLoading]=useState(false)
+
+  useEffect(()=>{
+setLoading(true)
+setTimeout(()=>{
+  setLoading(false)
+},8000)
+  },[])
   return (
     <div className="App">
-   <Navbar/>
+      {loading? <ClipLoader
+        color={"#FFCC00"}
+        loading={loading}
+        
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />:
+   <div><Navbar/>
    <InformationSection/>
    <Features/>
    <ChooseUs/>
    <Course/>
    <Testimonies/>
-  <Footer/>
+  <Footer/></div>}
     </div>
   );
 }
